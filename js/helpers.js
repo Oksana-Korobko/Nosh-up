25cb275a561bdb4a974ba2a419e2588b2fb2fed2
Oksana Korobko <oksana.korobko.fd@gmail.com>
2023-05-28 20:31:13 +0300

    Calculate total price

--- js/helpers.js
+++ js/helpers.js
@@ -1,8 +1,20 @@
+function createElement(elType, parent, text, attr) {
+    const element = document.createElement(elType);
+    if (elType === 'div') {
+        element.textContent = text
+    };
+    if (attr) {
+        element.setAttribute( `${attr}`, attr);
+    }
+    parent.appendChild(element);
+    return element;
+};
+
 function showGoods(event){
     numShop = event.target.getAttribute('number');
-    goodsList.textContent = "";
-    const goods = shops[numShop].goods;
-    goods.map(el => {
+    goods.textContent = "";
+    const goodsList = shops[numShop].goods;
+    goodsList.map(el => {
         goodsCard(el)
     })
     // goodsList.textContent = goods;
@@ -11,7 +23,6 @@ function goodsCard(article){
     const elem = document.createElement('div');
     elem.classList.add('goodsCard');
     elem.textContent = article.name;
-    // const img = document.createElement('img');
     const elemImg = document.createElement('div');
     elemImg.classList.add('goods__img');
     elemImg.insertAdjacentHTML('beforeend', '<img src="'+ article.url +'">');
@@ -19,20 +30,24 @@ function goodsCard(article){
     const elemPrice = document.createElement('div');
     elemPrice.textContent = "Price: " + article.price;
     elem.appendChild(elemPrice);
-
     btnCreate("add", article.id, elem, addToCart);
-
-    // elem.setAttribute('number', i);
-    goodsList.appendChild(elem);
-    // elem.addEventListener('click', showGoods)
-    
+    goods.appendChild(elem);
+      
 }
 
 function addToCart(event){
     const id = event.target.getAttribute('article');
-    cart.push(shops[numShop].goods[id]);
-    
-    console.log(cart)
+    // orderNum++;
+    // shops[numShop].goods[id-1].orderID = orderNum;
+    if (! cart.includes(shops[numShop].goods[id-1])) {
+        const order = shops[numShop].goods[id-1];
+        order.cost = shops[numShop].goods[id-1].price;
+        order.quantity = 1
+        cart.push(order);   
+        
+        console.log(cart)    
+    }
+   
 }
 
 function btnCreate (attr, article, parent, handler){
@@ -62,5 +77,78 @@ function createList(){
 function showShops(){
     ui.style.display = "none";
     order.style.display = "none";
+    goods.style.display = "block";
+    shopsList.style.display = "block";
+    shopsList.textContent = "";
     createList();
 }
+
+function showCart(){
+    ui.style.display = "block";
+    order.style.display = "block";
+    goods.style.display = "none";
+    shopsList.style.display = "none";
+    
+    showUserInfo();
+    showOrder();
+
+};
+// User Info
+function showUserInfo(){
+    ui.textContent = "";
+    const userForm = document.createElement('form');
+    createElement('div', userForm, 'Name', undefined);
+    createElement('input', userForm, undefined, 'uName');
+    createElement('div', userForm, 'Email', undefined);
+    createElement('input', userForm, undefined, 'uEmail');
+    createElement('div', userForm, 'Phone', undefined);
+    createElement('input', userForm, undefined, 'uPhone');
+    createElement('div', userForm, 'Address', undefined);
+    createElement('input', userForm, undefined, 'uAddress');
+    ui.appendChild(userForm);
+}
+
+function showOrder () {
+    ordersList.textContent = "";
+    for (let i = 0; i< cart.length; i++){
+        const numOrder = i;
+        const orderCard = createElement('div', ordersList);
+        const elemImg = document.createElement('div');
+        elemImg.classList.add('orders__img');
+        orderCard.appendChild(elemImg);
+        elemImg.insertAdjacentHTML('beforeend', '<img src="'+ cart[i].url +'">');
+        const orderInfo = createElement('div', orderCard);
+        createElement('div', orderInfo, cart[i].name);        
+        createElement('div', orderInfo, 'Quantity: ');
+        
+      
+        const quantityInput = createElement('input',orderInfo);  
+        quantityInput.setAttribute('numOrder', numOrder);
+        quantityInput.value =  cart[i].quantity;
+        
+        // добавть цену за 1 продукт ----------------------------------------!
+        quantityInput.addEventListener('change', costCalc);
+        
+    }
+   
+}
+
+function costCalc(event){
+    const orderNumber = event.target.getAttribute('numOrder');
+
+    // cart[orderNumber].price = shops[orderNumber].price  * event.target.value;
+    cart[orderNumber].cost = Number(cart[orderNumber].price) * Number(event.target.value);
+    console.log(cart[orderNumber].cost )
+    cart[orderNumber].quantity = event.target.value
+
+    totalPrice = 0;
+    for (let i = 0; i<cart.length; i++){
+        totalPrice += Number(cart[i].cost);
+    }        
+    const costTotal = document.querySelector('#price');
+    costTotal.textContent = totalPrice;
+
+
+    
+}
+
--- js/main.js
+++ js/main.js
@@ -7,25 +7,25 @@ const shops = [
                "id": 1,
                "name": "Classic Pizza",
                "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/8/23/0/FNM_100116-Classic-Crust_s4x3.jpg.rend.hgtvcom.826.620.suffix/1480972867043.jpeg", 
-               "price": '$10' 
+               "price": 10
             },
             {
                "id": 2, 
                "name": "Pineapple Glazed Ham",
                 "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/04/06/FNK_Pineapple-Apricot-Glazed-Ham_H_4x3.jpg.rend.hgtvcom.826.620.suffix/1586200112476.jpeg" , 
-                "price": '$12' 
+                "price": 12 
              },
              {
                "id": 3, 
                "name": "Classic Shrimp Scampi",
                 "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/07/16/0/FNM_090120-Classic-Shrimp-Scampi_s4x3.jpg.rend.hgtvcom.826.620.suffix/1594915956100.jpeg", 
-                "price": '$13'  
+                "price": 13  
              },
              {
                "id": 4, 
                "name": "Linguine and Clams",
                 "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/8/15/0/CASH_1_s4x3.jpg.rend.hgtvcom.826.620.suffix/1408128762992.jpeg" , 
-                "price": '$15' 
+                "price": 15
              }
              
         ]
@@ -37,26 +37,26 @@ const shops = [
                "id": 1,
                "name": "Garlic Burger Patties",
                "url": "https://www.thespruceeats.com/thmb/RggxXasgBcaHExtv4ZInuFvJbkQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/garlic-burger-patties-333503-hero-01-e4df660ff27b4e5194fdff6d703a4f83.jpg", 
-               "price": '$8'  
+               "price": 8  
             },
             {
                "id": 2, 
                "name": "Butter Burgers",
                 "url": "https://www.thespruceeats.com/thmb/LXtz21QVUFStKKa9qxmYGX-_EEg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/butter-burger-4799859-hero-01-fe7fb6a623524539a6d52185160dfe94.jpg", 
-                "price": '$12'  
+                "price": 12  
              },
              {
                "id": 3, 
                "name": "Mini Burgers",
                 "url": "https://www.thespruceeats.com/thmb/8aOa2rgIMk8CLCvSTJvXI1ia-O0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-american-sliders-479161-hero-04-8b32f8aa35ad462e9a8c701a1beddea3.jpg" , 
-                "price": '$11' 
+                "price": 11 
              },
              {
                 
                "id": 4,
                "name": "Hamburger Hot Dogs",
                 "url": "https://www.thespruceeats.com/thmb/cEXszR5dA2c4cCeoW59RfPjS9f8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/hamburger-hot-dogs-480688-hero-01-806dc82af5a44b3ea3704e298b7c81c1.jpg" , 
-                "price": '$9' 
+                "price": 9 
              }
              
         ]
@@ -69,11 +69,20 @@ const shops = [
 const cart = [];
 const ui = document.querySelector('.userInfo');
 const order = document.querySelector('.order');
-const goodsList = document.querySelector('.goods');
+const goods = document.querySelector('.goods');
+const shopsList = document.querySelector('.shopsList');
+const ordersList = document.querySelector('.ordersList');
+const navShop = document.querySelector('.shop');
+const navCart = document.querySelector('.cart');
+let orderNum = 0;
 let numShop;
+
+let totalPrice = 0;
 //  is LocalStorage empty?
 if (localStorage.length === 0) {
    localStorage.setItem('shops', JSON.stringify(shops));
 };
 
-showShops();
+navShop.addEventListener('click', showShops);
+navCart.addEventListener('click', showCart);
+
