1e3fc317fa794ab61a7866aa3c7364405a19a95b
Oksana Korobko <oksana.korobko.fd@gmail.com>
2023-06-04 22:28:12 +0300

    LocalStorage - history

--- js/main.js
+++ js/main.js
@@ -1,178 +1,3 @@
-const shops = [
-    {
-        "name": "Summer Treat",
-        "goods": [
-            {
-               "id": 1,
-               "name": "Classic Pizza",
-               "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/8/23/0/FNM_100116-Classic-Crust_s4x3.jpg.rend.hgtvcom.826.620.suffix/1480972867043.jpeg", 
-               "price": 10
-            },
-            {
-               "id": 2, 
-               "name": "Pineapple Glazed Ham",
-                "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/04/06/FNK_Pineapple-Apricot-Glazed-Ham_H_4x3.jpg.rend.hgtvcom.826.620.suffix/1586200112476.jpeg" , 
-                "price": 12 
-             },
-             {
-               "id": 3, 
-               "name": "Quorn Vegan Quiche",
-                "url": "https://images.ctfassets.net/uexfe9h31g3m/1itnlWpRu57DPn3r0z9voP/091ff09f8ee268c46aac315c2c0c6ab7/Vegan_quiche_4X3_crop_copy.jpg?w=768&h=576&fm=webp&fit=thumb&q=90", 
-                "price": 13  
-             },
-             {
-               "id": 4, 
-               "name": "Mini Quiche Lorraine",
-                "url": "https://images.ctfassets.net/uexfe9h31g3m/3bm2oFUgnSQsSUko4aI6WM/87374424a59aca6e284ae34fd142b659/Veggie_Quiche_Lorraine-Insta_3_copy.jpg?w=768&h=576&fm=webp&fit=thumb&q=90" , 
-                "price": 9
-             }
-             
-        ]
-    },
-    {
-        "name": "Epic",
-        "goods": [
-            {
-               "id": 1,
-               "name": "Garlic Burger Patties",
-               "url": "https://www.thespruceeats.com/thmb/RggxXasgBcaHExtv4ZInuFvJbkQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/garlic-burger-patties-333503-hero-01-e4df660ff27b4e5194fdff6d703a4f83.jpg", 
-               "price": 8  
-            },
-            {
-               "id": 2, 
-               "name": "Butter Burgers",
-                "url": "https://www.thespruceeats.com/thmb/LXtz21QVUFStKKa9qxmYGX-_EEg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/butter-burger-4799859-hero-01-fe7fb6a623524539a6d52185160dfe94.jpg", 
-                "price": 12  
-             },
-             {
-               "id": 3, 
-               "name": "Mini Burgers",
-                "url": "https://www.thespruceeats.com/thmb/8aOa2rgIMk8CLCvSTJvXI1ia-O0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/all-american-sliders-479161-hero-04-8b32f8aa35ad462e9a8c701a1beddea3.jpg" , 
-                "price": 11 
-             },
-             {
-                
-               "id": 4,
-               "name": "Hamburger Hot Dogs",
-                "url": "https://www.thespruceeats.com/thmb/cEXszR5dA2c4cCeoW59RfPjS9f8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/hamburger-hot-dogs-480688-hero-01-806dc82af5a44b3ea3704e298b7c81c1.jpg" , 
-                "price": 9 
-             }
-             
-        ]
-    },
-    {
-      "name": "Neko sushi",
-      "goods": [
-          {
-             "id": 1,
-             "name": "TEMPURA MIX",
-             "url": "https://neko.ee/wp-content/uploads/2022/10/0B7A1330-%D1%80%D0%B5%D0%B4%D0%B0%D0%BA%D1%82-1-968x646.jpg", 
-             "price": 10
-          },
-          {
-             "id": 2, 
-             "name": "TUNA TATAKI",
-              "url": "https://neko.ee/wp-content/uploads/2016/11/IMG_34031.jpg" , 
-              "price": 12 
-           },
-           {
-             "id": 3, 
-             "name": "TEMPURA KING PRAWNS",
-              "url": "https://neko.ee/wp-content/uploads/2015/09/IMG_69812-665x443.jpg", 
-              "price": 13  
-           },
-           {
-             "id": 4, 
-             "name": "SPRING ROLLS",
-              "url": "https://neko.ee/wp-content/uploads/2015/09/IMG_69771-665x443.jpg" , 
-              "price": 7
-           },
-           {
-            "id": 5, 
-            "name": "SEAFOOD SALAD",
-             "url": "https://neko.ee/wp-content/uploads/2014/05/Neko-235_52-665x443.jpg" , 
-             "price": 15
-          },
-          {
-            "id": 6, 
-            "name": "MISO SOUP",
-             "url": "https://neko.ee/wp-content/uploads/2013/10/IMG_0067_3-Nom.232-665x443.jpg" , 
-             "price": 15
-          },
-           
-      ]
-  },
-  {
-   "name": "Good Afternoon",
-   "goods": [
-       {
-          "id": 1,
-          "name": "Grilled S’mores Pie",
-          "url": "https://www.foodandwine.com/thmb/OtnuD9dfVNsNWws-oivpIEUUzi4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Grilled-Smores-Pie-FT-RECIPE1022-200-145d1a57e8ef4375a131bcfcaf22c395.jpg", 
-          "price": 10
-       },
-       {
-          "id": 2, 
-          "name": "Tamarind Chicken",
-           "url": "https://www.foodandwine.com/thmb/nDBGGuZzVOHIUkLT_fWorNv3nS8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Tamarind-Chicken-FT-RECIPE0522-80072d93f7bc4bc7abf1dcf5b5317b0c.jpg" , 
-           "price": 15 
-        },
-        {
-          "id": 3, 
-          "name": "Santa Maria BBQ",
-           "url": "https://www.foodandwine.com/thmb/HGhgKqMTNwSON3LcbPCov8BKq_0=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Traditional-Santa-Maria-BBQ-Oak-Wood-Grilled-Tri-Tip-FT-recipe0619-48c62f7da66c4a9aaea81b599a0846de.jpg", 
-           "price": 12  
-        },
-        {
-          "id": 4, 
-          "name": "Citrus Chicken",
-           "url": "https://www.foodandwine.com/thmb/cw8nSh_2cl5KPBMTbfjlv11bzRg=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/citrus-and-fennel-chicken-with-olives-and-calabrian-chiles-FT-RECIPE0222-c1ef1d87fa514fa08e1616617cdab618.jpg" , 
-           "price": 13
-        },
-        {
-         "id": 5, 
-         "name": "Lamb Leg Steak",
-          "url": "https://www.foodandwine.com/thmb/sSj57pkuyCqYz6LpzoSCLpxBOJ0=/282x188/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/lamb-leg-steak-with-pistachio-gremolata-and-marsala-onions-ft-RECIPE0718-de112b36e7f646cda24e6934bfc290f6.jpg" , 
-          "price": 16
-       }
-        
-   ]
-},
-{
-   "name": "DELI FOOD",
-   "goods": [
-       {
-          "id": 1,
-          "name": "Ultimate Ham Sandwich",
-          "url": "https://images.ctfassets.net/uexfe9h31g3m/30PK5LALEpgExQcAR9j522/7becc83c5c40312d3e81acbe5dbeb44a/Quorn_UK_deli_recipe_yorkshire-ham_1024x768.jpg?w=768&h=576&fm=webp&fit=thumb&q=90", 
-          "price": 7
-       },
-       {
-          "id": 2, 
-          "name": "Caesar Salad Sandwich",
-           "url": "https://images.ctfassets.net/uexfe9h31g3m/5nJ88WhXEePmcqUMs76nCr/51e61ff1ba65b8428753b8f0c13ffa76/Quorn_UK_deli_recipe_roast-chicken_1024x768.jpg?w=768&h=576&fm=webp&fit=thumb&q=90" , 
-           "price": 12 
-        },
-        {
-          "id": 3, 
-          "name": "Classic Shrimp Scampi",
-           "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/07/16/0/FNM_090120-Classic-Shrimp-Scampi_s4x3.jpg.rend.hgtvcom.826.620.suffix/1594915956100.jpeg", 
-           "price": 13  
-        },
-        {
-          "id": 4, 
-          "name": "Linguine and Clams",
-           "url": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/8/15/0/CASH_1_s4x3.jpg.rend.hgtvcom.826.620.suffix/1408128762992.jpeg" , 
-           "price": 15
-        }
-        
-   ]
-},
-
-    
-    
-];
-
 let cart = [];
 let orders = [];
 const ui = document.querySelector('.userInfo');
@@ -186,20 +11,20 @@ const costTotal = document.querySelector('#price');
 const orderTitle = document.querySelector('.orderTitle');
 const orderTotal = document.querySelector('.orderTotal');
 const sliderBlock =  document.querySelector('.slider');
-const history = [];
+let history = [];
 
 
 let orderNum = 0;
 let numShop;
 showSlider();
 let totalPrice = 0;
-//  is LocalStorage empty?
-if (localStorage.length === 0) {
-   localStorage.setItem('shops', JSON.stringify(shops));
+//  is LocalStorage fuel?
+
+if (localStorage.length) {
+   history = JSON.parse(localStorage.getItem('history'))
+     
 };
-
 showShops();
-
 navShop.addEventListener('click', showShops);
 navCart.addEventListener('click', showCart);
 
--- shop.js
+++ shop.js
@@ -172,4 +172,3 @@ const shops = [
    
    
 ];
-export default shops;
